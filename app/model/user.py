2f0bb6b8085a3412f4986a4d851bc06492ffcbec
Đạt Bê <90543186+Datbe2001@users.noreply.github.com>
2023-09-17 20:42:36 +0700

    feat: setup directory for system and crud for user

---
+++ app/model/user.py
@@ -0,0 +1,30 @@
+from sqlalchemy import (Boolean, Column, Date, String, text, JSON, Integer, func, TIMESTAMP)
+from sqlalchemy.orm import relationship
+
+from app.model.base import Base, UserSystemRole
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(String(255), primary_key=True)
+    username = Column(String(42), nullable=False)
+    full_name = Column(String(42), nullable=True)
+    avatar = Column(String(255), nullable=True)
+    phone = Column(String(255), nullable=True)
+    email = Column(String(255), nullable=False)
+    birthday = Column(Date, nullable=True)
+    is_active = Column(Boolean, nullable=False, server_default=text("false"))
+    hashed_password = Column(String(255), nullable=True)
+    verify_code = Column(String(255), nullable=True)
+    qr_code = Column(String(255), nullable=True)
+    system_role = Column(String(255), nullable=False, default=UserSystemRole.MEMBER)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text("now()"))
+    updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"),
+                        onupdate=func.current_timestamp())
+
+    # comments = relationship("Comment", back_populates="user", passive_deletes=True)
+    # courses = relationship("Course", back_populates="user", passive_deletes=True)
+    # user_courses = relationship("UserCourse", back_populates="user", passive_deletes=True)
+    # reply_comments = relationship("ReplyComment", back_populates="user", passive_deletes=True)
+    # notifications = relationship("Notification", back_populates="user", passive_deletes=True)
