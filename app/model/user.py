ca2375527f4c0e126376f4629c8b9e8c788e5e65
Đạt Bê <90543186+Datbe2001@users.noreply.github.com>
2023-11-12 14:41:53 +0700

    fix: response user when call transaction

--- app/model/user.py
+++ app/model/user.py
@@ -37,6 +37,6 @@ class User(Base):
     financial_transactions = relationship("FinancialTransaction", back_populates="user", passive_deletes=True)
     leaderboards = relationship("Leaderboard", back_populates="user", passive_deletes=True)
     carts = relationship("Cart", back_populates="user", passive_deletes=True)
-    # transactions_fm = relationship("TransactionFM", back_populates="user", passive_deletes=True)
-    # transactions_sf = relationship("TransactionSF", back_populates="user", passive_deletes=True)
+    transaction_fm = relationship("TransactionFM", back_populates="user", passive_deletes=True)
+    transaction_sf = relationship("TransactionSF", back_populates="user", passive_deletes=True)
     # marketplace = relationship("Marketplace", back_populates="user", passive_deletes=True)
