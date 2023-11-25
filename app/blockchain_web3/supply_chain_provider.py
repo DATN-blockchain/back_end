3ee64f7d4ac43f5d4b944acbb12b0707482fa28c
Duy Luan <nvdluan@gmail.com>
2023-11-25 16:27:10 +0700

    feature update status, withdraw, deposited of user on bockchain

--- app/blockchain_web3/supply_chain_provider.py
+++ app/blockchain_web3/supply_chain_provider.py
@@ -26,25 +26,24 @@ class SupplyChainProvider(Web3Provider):
 
     def listing_product_to_marketplace(self, item_id, product_id, owner):
         function = self.contract.functions.listing_product(item_id, product_id, owner)
-        tx_hash = self.sign_and_send_transaction(function)
-        return tx_hash
+        return self.sign_and_send_transaction(function)
 
     def buy_product_in_market(self, product_id, id_trans, buyer, quantity, type_product):
         function = self.contract.functions.buy_item_on_marketplace(product_id, buyer, id_trans, quantity, type_product)
-        tx_hash = self.sign_and_send_transaction(function)
-        return tx_hash
+        return self.sign_and_send_transaction(function)
 
-    def update_status_item_on_marketplace(self, item_id, status):
-        function = self.contract.functions.update_item_on_marketplance(item_id, status)
-        tx_hash = self.sign_and_send_transaction(function)
-        return tx_hash
+    def update_transaction_status(self, item_id, status):
+        function = self.contract.functions.update_status_transaction(item_id, status)
+        return self.sign_and_send_transaction(function)
 
     def get_transaction_by_id(self, trans_id):
         return self.contract.functions.get_transaction_by_id(trans_id).call()
+
 
 if __name__ == "__main__":
     provider = SupplyChainProvider()
     product = provider.get_info_product(product_id="3b9fc13a-4bd6-4ed8-a761-67e404d53dcd")
     print(product)
-    tx_hash = provider.listing_product_to_marketplace(uuid.uuid4().__str__(), "3b9fc13a-4bd6-4ed8-a761-67e404d53dcd", "a98c4ce8-2fee-4ad4-9652-8848f3afe50d")
+    tx_hash = provider.listing_product_to_marketplace(uuid.uuid4().__str__(), "3b9fc13a-4bd6-4ed8-a761-67e404d53dcd",
+                                                      "a98c4ce8-2fee-4ad4-9652-8848f3afe50d")
     print(tx_hash)
