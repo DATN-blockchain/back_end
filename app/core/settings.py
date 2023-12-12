03263043ffc9559ed55348b8d4ad834f0551d8b7
Đạt Bê <90543186+Datbe2001@users.noreply.github.com>
2023-12-12 14:57:53 +0700

    fix: update trace origin

--- app/core/settings.py
+++ app/core/settings.py
@@ -45,6 +45,12 @@ class Settings(BaseSettings):
     PRIVATE_KEY_SYSTEM: Optional[str] = None
     CHAIN_ID: Optional[str] = 421613
     HASH_KEY: Optional[str] = None
+    # WEB3_PROVIDER = "https://goerli-rollup.arbitrum.io/rpc"
+    # ADDRESS_CONTRACT_ACTOR_MANAGER ="0xADC3E1AAb3660cEf413822950101e4680d2E52C9"
+    # ADDRESS_CONTRACT_PRODUCT_MANAGER = "0x17C486dc7AeCFe07acE15BD142e2bBE22892f5A2"
+    # ADDRESS_CONTRACT_SUPPLY_CHAIN = "0xdA86b46a57Da7D9017Cd76B5B8fAC2Eb3f8DaA1b"
+    # CHAIN_ID = 421613
+    # PRIVATE_KEY_SYSTEM = "2bd81d7cace245abc1a7e981075332251823b56a136c0154187cd8a0746ed84a"
 
     # VNPAY CONFIG
     VNPAY_RETURN_URL: Optional[str] = None  # get from config
